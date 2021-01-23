0c64c6927ac8d4a6a41c5ba69114b1320a49ac2f
LuismiGarcia <77366562+LuismiGarcia@users.noreply.github.com>
2021-01-24 00:53:44 +0100

    Update Constantes.php
    Tercera modificación. Esta es la primera desde github, no desde netbeans

--- Juego_Colores_v1/clases/Constantes.php
+++ Juego_Colores_v1/clases/Constantes.php
@@ -5,7 +5,8 @@ class Constantes
 {
     /**
      * una constante con los colores del juego
-     * Tenerlo aquí nos va a facilitar acceder a ellos desde cualquier fichero que lo necesite
+     * Tenerlo aquí nos va a facilitar acceder a ellos desde cualquier fichero que lo necesite.
+     * También en vez de usar esta clase se puede utilizar un array con los colores y trabajar diréctamente con él
      */
     const COLORES =['Azul', 'Rojo', 'Naranja',
         'Verde', 'Violeta', 'Amarillo',
